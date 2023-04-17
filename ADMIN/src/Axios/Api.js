167e17fdb3ae2b7190a454fd9245825115f19c1c
WafaZouari <wafazouari14@gmail.com>
2023-04-17 15:18:32 +0200

    new admin build

--- ADMIN/src/Axios/Api.js
+++ ADMIN/src/Axios/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const url = "http://localhost:3003/api"
+const url = "https://mascotte-api.onrender.com/api" 
 //const url=process.env.REACT_APP_ADRESSE+"api";  si en utulise .env
 export default axios.create({
     baseURL: url
 })
 
-// http://localhost:8800/articles
+// http://localhost:8800/articles / "http://localhost:3003/api"
