da97521da1933e864cc1f5644ec5cddd81f7e1df
Mohamed HAZAMI <meed.hazami@gmail.com>
2023-08-22 13:13:33 +0100

    build manualy1

--- MENU/src/Axios/Api.js
+++ MENU/src/Axios/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 export default axios.create({
-   baseURL: process.env.REACT_APP_API
+//    baseURL: process.env.REACT_APP_API
+      baseURL:  "https://mascotte-api.onrender.com/api/"
 })
 
 // export default axios.create({
